b0e87e52613dcfdeccf57b786a317199b7f1be77
HirayamaSachi <hirayamas0305@gmail.com>
2024-09-15 19:09:52 +0900

    section6-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,34 @@
 'use client'
-import { useSearchParams } from "../../node_modules/next/navigation"
 
-export default async function Home() {
-  const searchParams = useSearchParams()
+import {useState} from 'react'
+
+const url = 'http://localhost:3000/sample.json'
+
+async function getSampleData() {
+  const resp = await fetch(
+    url,
+    {cache: 'no-store'}
+  )
+  const result = resp.json()
+  return result
+}
+
+export default function Home() {
+  const [msg, setMsg] = useState("dummy message")
+  const doAction = () =>{
+    getSampleData().then(resp => {
+      setMsg(resp.message)
+    })
+  }
+
   return (
     <main>
-      <h1 className="title">index page</h1>
-      <ul>パラメータ
-        <li>{searchParams.get('id')}</li>
-        <li>{searchParams.get('pass')}</li>
-      </ul>
+      <h1 className="title">Index page</h1>
+      <p className="msg">{msg}</p>
+      <div className="form">
+        <button className="btn" onClick={doAction}>Click</button>
+      </div>
+
     </main>
   )
 }
