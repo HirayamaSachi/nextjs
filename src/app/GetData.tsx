3574c456fba3457471fcb0809431f67a95f05111
HirayamaSachi <hirayamas0305@gmail.com>
2024-09-29 17:45:50 +0900

    fix:typo

--- src/app/GetData.tsx
+++ src/app/GetData.tsx
@@ -5,7 +5,7 @@ const url = '/sample.json'
 const fetcher = (url: string)=>fetch(url).then(res=>res.json())
 
 export default function GetData() {
-    const {data,_error,_isLoading} = useSWR(url,fetcher)
+    const {data, error, isLoading} = useSWR(url,fetcher)
     return (
         data ?
         <p className="msg border p-2">{data.message}</p>
