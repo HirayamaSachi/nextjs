37986474b2fd493e060e7fbf393ae62e8bd7e819
HirayamaSachi <hirayamas0305@gmail.com>
2024-10-08 23:51:06 +0900

    fix:loadingを追加

--- src/app/todo/read/page.tsx
+++ src/app/todo/read/page.tsx
@@ -1,22 +1,24 @@
 'use client'
 import { useEffect, useState } from 'react'
 import { readTodo } from '@/app/server-action'
-
+import { QueryResultRow } from '../../../../node_modules/@vercel/postgres/dist/index.cjs'
 
 export default function Todo()  {
     const [todos, setTodos] = useState([])
+    const [loading, setLoading] = useState(true)
     useEffect(()=>{
         readTodo().then(res => setTodos(res))
-
+        setLoading(false)
     }, [])
 
     return (
         <div>
             {
+                loading ?
+                "取得中" : 
                 todos.map((todo) => 
                 <div key={todo.id}>
                     <p>{todo.finished ? '[x]' : '[ ]'}{todo.name}</p>
-                    <label>{}</label>
                 </div>
                 )
             }
