0192da577e0235823cf67e15a2eb7e653c480ac4
HirayamaSachi <hirayamas0305@gmail.com>
2024-10-08 23:31:08 +0900

    fix:一覧修正

--- src/app/todo/read/page.tsx
+++ src/app/todo/read/page.tsx
@@ -1,22 +1,13 @@
 'use client'
-import { useEffect, useState } from 'react';
-import useSWR from 'swr'
+import { useEffect, useState } from 'react'
+import { readTodo } from '@/app/server-action'
 
 
 export default function Todo()  {
     const [todos, setTodos] = useState([])
     useEffect(()=>{
-        const fetchTodos = async () => {
-            try {
-                const res = await fetch('/todos.js')
-                console.log(res)
-                // const data = await res.json()
-                // setTodos(data)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        fetchTodos()
+        readTodo().then(res => setTodos(res))
+
     }, [])
 
     return (
@@ -24,8 +15,8 @@ export default function Todo()  {
             {
                 todos.map((todo) => 
                 <div key={todo.id}>
-                    <input type="checkbox" name="finished" id={todo.id} defaultChecked={todo.finished ? true : false}/>
-                    <label>{todo.name}</label>
+                    <p>{todo.finished ? '[x]' : '[ ]'}{todo.name}</p>
+                    <label>{}</label>
                 </div>
                 )
             }
