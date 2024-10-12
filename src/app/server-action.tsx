2dd16471bac2d630b159a2677626892f3b97ba6a
HirayamaSachi <hirayamas0305@gmail.com>
2024-10-12 17:59:43 +0900

    remove:tmpのコメントアウト箇所

--- src/app/server-action.tsx
+++ src/app/server-action.tsx
@@ -35,10 +35,8 @@ export async function createTodo (form: FormData) {
 }
 
 export async function readTodo (page? : number) {
-    // todo:paginatorつける
     const offset = (page && page > 0) ? (page - 1) * 5 : 0
     const data = await sql`SELECT * FROM todo LIMIT 5 OFFSET ${offset}`
-    // const data = await sql`SELECT * FROM todo`
     return data.rows
 
 }
