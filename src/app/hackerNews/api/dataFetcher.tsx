9eac014b58691e1f6884129ee14659546a2f1d5d
HirayamaSachi <hirayamas0305@gmail.com>
2024-11-12 23:47:06 +0900

    update:APIデータをPPRで取得

--- src/app/hackerNews/api/dataFetcher.tsx
+++ src/app/hackerNews/api/dataFetcher.tsx
@@ -2,30 +2,48 @@
 import useSWR from 'swr'
 
 const BASE_API_URL = 'https://hacker-news.firebaseio.com/v0/'
-function AllFetcher(...urls: string[][]) {
+function allFetcher(...urls: string[][]) {
     const f = (url: string) => fetch(url).then(r => r.json())
     if (!Array.isArray(urls)) return undefined
     return Promise.all(urls[0].map(f))
 }
 
-function Fetcher(...urls: [string]) {
+function fetcher(...urls: [string]) {
     return fetch(...urls).then(res => res.json())
 }
 
-export function GetTopNewsIds() {
-    const { data, error, isLoading } = useSWR(BASE_API_URL + 'topstories.json?print=pretty', Fetcher)
-    return {
-        data: data ? data : [],
-        error: error ? error : false,
-        isLoading: isLoading ? isLoading : false,
-    }
-}
+export function TopNewsDetail() {
+    // apiの取得
+    const { data: newsIds, error: idsError, isLoading: idsIsLoading } = useSWR(BASE_API_URL + 'topstories.json?print=pretty', fetcher)
+    const dataUrls = newsIds ? newsIds.slice(0,5).map((id : number, _ :number) => {return BASE_API_URL + `item/${id}.json?print=pretty`}) : []
+    const { data: newsData, error: dataError } = useSWR(dataUrls, allFetcher)
 
-export function GetNewsDetail(ids : string[]) {
-    const dataUrls = ids.slice(0,5).map((id, _) => {return BASE_API_URL + `item/${id}.json?print=pretty`}) 
-    const { data, error } = useSWR(dataUrls, AllFetcher)
-    return {
-        data: data ? data : [],
-        error: error ? error : false,
+
+    if (idsError || dataError) return <div>failed to load</div>
+    if (idsIsLoading) return <div>loading...</div>
+    if (!Array.isArray(newsIds)) {
+        return <div>newsIds is loading...</div>
     }
+    if (!newsData) {
+        return <div>newsData loading...</div>
+    }
+    if (Array.isArray(newsData)) return (
+        <div>
+            {newsData.map((item, key) => {
+                const dateTime = new Date(item.time * 1000)
+                const commentCnt = Array.isArray(item.kids) ? item.kids.length : 0
+                return (
+                    <div className='p-1' key={key}>
+                        <h1>{item.title}</h1>
+                        <div className='flex'>
+                            <p className='p-0.5'>By {item.by}</p>
+                            <p className='p-0.5'>{dateTime.toLocaleDateString()}</p>
+                            <p className='p-0.5'>{commentCnt} comments</p>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+
 }
--- src/app/hackerNews/page.tsx
+++ src/app/hackerNews/page.tsx
@@ -1,34 +1,19 @@
 'use client'
-import { GetTopNewsIds, GetNewsDetail} from './api/dataFetcher'
+import {TopNewsDetail} from './api/dataFetcher'
+import { Suspense} from 'react'
 
 
 export default function App() {
-    const {data : newsIds, error: idsError, isLoading: idsIsLoading} = GetTopNewsIds()
-    const {data : newsData, error: dataError} = GetNewsDetail(Array.isArray(newsIds) ? newsIds : [])
-    if(idsError || dataError) return <div>failed to load</div>
-    if(idsIsLoading) return <div>loading...</div>
-    if(!Array.isArray(newsIds)) {
-        return <div>newsIds is loading...</div>
-    }
-    if(!newsData) {
-        return <div>newsData loading...</div>
-    }
-    if(Array.isArray(newsData)) return (
+    return (
         <div>
-            {newsData.map((item, key) => {
-                const dateTime = new Date(item.time * 1000)
-                const commentCnt = Array.isArray(item.kids) ? item.kids.length : 0
-                return (
-                    <div className='p-1' key={key}>
-                        <h1>{item.title}</h1>
-                        <div className='flex'>
-                            <p className='p-0.5'>By {item.by}</p>
-                            <p className='p-0.5'>{dateTime.toLocaleDateString()}</p>
-                            <p className='p-0.5'>{commentCnt} comments</p>
-                        </div>
-                    </div>
-                )
-            })}
+            <h1>Hacker NEWS!</h1>
+            <div className='flex'>
+                
+            </div>
+            <Suspense>
+                <TopNewsDetail />
+            </Suspense>
         </div>
     )
+
 }
