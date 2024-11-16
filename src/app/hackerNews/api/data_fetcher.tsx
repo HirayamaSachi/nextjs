5e21d56b2eb0604e232db9e36dc631b5478184ac
HirayamaSachi <hirayamas0305@gmail.com>
2024-11-16 20:21:49 +0900

    fix:2回目はキャッシュから取得させる

--- src/app/hackerNews/api/data_fetcher.tsx
+++ src/app/hackerNews/api/data_fetcher.tsx
@@ -1,10 +1,10 @@
 export const BASE_API_URL = 'https://hacker-news.firebaseio.com/v0/'
 export function allFetcher(...urls: string[][]) {
-    const f = (url: string) => fetch(url).then(r => r.json())
+    const f = (url: string) => fetch(url, {cache:"no-store"}).then(r => r.json())
     if (!Array.isArray(urls)) return undefined
     return Promise.all(urls[0].map(f))
 }
 
 export function fetcher(...urls: [string]) {
-    return fetch(...urls).then(res => res.json())
+    return fetch(...urls, {cache:"no-store"}).then(res => res.json())
 }
