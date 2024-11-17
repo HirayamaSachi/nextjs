93e6f1cd7ff479f21ec6087b3b2c75d6405ec96e
HirayamaSachi <hirayamas0305@gmail.com>
2024-11-17 23:03:10 +0900

    add:コメント toggle設置

--- src/app/hackerNews/api/NewsDetail.tsx
+++ src/app/hackerNews/api/NewsDetail.tsx
@@ -1,12 +1,27 @@
 'use client'
 import { fetcher, allFetcher, BASE_API_URL } from './data_fetcher'
+import {CommentDetail} from './CommentDetail'
 import useSWR from 'swr'
+import { useState } from 'react'
 
 
 interface Props {
     url: string
 }
 export function NewsDetail(props : Props) {
+    const [openIds, setOpenIds] = useState<number[]>([])
+    let latestOpenIds = openIds
+    const toggleItem = (changeId: number) => {
+        if (openIds.includes(changeId)) {
+            // setOpenIdsを使用した時点でopenIdsが新しいデータに反映されるわけではないようなのでlatestOpenIdsに代入
+            // setOpenIdsは再レンダリングを呼ぶだけなので、更新後の値は次のレンダリングでのみ使用できる
+            latestOpenIds = openIds.filter(id => id !== changeId)
+            setOpenIds(latestOpenIds)
+        } else {
+            latestOpenIds = [...openIds, changeId]
+            setOpenIds(latestOpenIds)
+        }
+    }
     const { data: newsIds, error: idsError, isLoading: idsIsLoading } = useSWR(BASE_API_URL + props.url, fetcher)
     const dataUrls = newsIds ? newsIds.slice(0, 5).map((item: number, _: number) => { return BASE_API_URL + `item/${item}.json?print=pretty` }) : ''
     const { data: newsData, error: dataError } = useSWR(dataUrls, allFetcher)
@@ -32,6 +47,9 @@ export function NewsDetail(props : Props) {
                             <p className='p-0.5'>{dateTime.toLocaleDateString()}</p>
                             <p className='p-0.5'>/</p>
                             <p className='p-0.5'>{commentCnt} comments</p>
+                            <button onClick={() => toggleItem(key)}>+ comment</button>
+                            {latestOpenIds.includes(key) && (<div><CommentDetail ids={item.kids ? item.kids : []} /></div>)}
+
                         </div>
                     </div>
                 )
