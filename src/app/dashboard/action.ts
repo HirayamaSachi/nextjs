570b77c1d162d59b4512315cbb960ad9582f94d8
HirayamaSachi <hirayamas0305@gmail.com>
2024-12-03 22:56:37 +0900

    add:validation機能

--- src/app/dashboard/action.ts
+++ src/app/dashboard/action.ts
@@ -3,29 +3,38 @@ import { redirect } from 'next/navigation'
 import { sql } from "@vercel/postgres"
 import { revalidatePath } from "next/cache";
 import { z } from "zod"
+export type FormState = {
+    name?: string,
+    email?: string,
+    password?: string,
+}
 
-export async function createUser(prevState: {message: string}, formData: FormData) {
+export async function createUser(prevState: FormState, formData: FormData) {
     const schema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string()
+        name: z.string().min(1, "nameの入力は必須です"),
+        email: z.string().min(1, "emailの入力は必須です").email("メールアドレスを入力してください"),
+        password: z.string().min(8, "8文字以上で入力してください")
     })
     const parse = schema.safeParse({
         name: formData.get("name"),
         email: formData.get("email"),
         password: formData.get("password"),
     })
-    if(!parse.success) {
-        return { message: `validation error`};
+    if (!parse.success) {
+        const formatted = parse.error.format()
+        return {
+            name: formatted.name?._errors.join(", "),
+            email: formatted.email?._errors.join(", "),
+            password: formatted.password?._errors.join(", "),
+        };
     }
     const data = parse.data;
     try {
         await sql`INSERT INTO users (name, email, password) VALUES (${data.name}, ${data.email}, ${data.password})`;
 
-    } catch (e){
-        throw new Error( `br>Failed to create User: ${e}` )
+    } catch (e) {
+        throw new Error(`br>Failed to create User: ${e}`)
     }
     revalidatePath('/dashboard')
     redirect('/dashboard')
-
 }
--- src/app/dashboard/create/page.tsx
+++ src/app/dashboard/create/page.tsx
@@ -4,7 +4,9 @@ import { useFormState } from 'react-dom'
 import { useFormStatus } from 'react-dom'
 import { createUser } from '@/app/dashboard/action'
 const initialState = {
-    message : ''
+    name: '',
+    email: '',
+    password: '',
 }
 
 export default function Create() {
@@ -13,19 +15,30 @@ export default function Create() {
         <div>
             <h1>CREATE USER</h1>
             <form action={formAction}>
-                <input type="text" name="name" />
-                <input type="text" name="email" />
-                <input type="text" name="password" />
-                <p aria-live='polite'>{state?.message}</p>
+                <div className='p-1'>
+                    <label htmlFor="name">名前</label>
+                    <input type="text" name="name" id="name" />
+                    <p className='text-red-500' aria-live='polite'>{state?.name}</p>
+                </div>
+                <div className='p-1'>
+                    <label htmlFor="email">メールアドレス</label>
+                    <input type="text" name="email" id="email" />
+                    <p className='text-red-500' aria-live='polite'>{state?.email}</p>
+                </div>
+                <div className='p-1'>
+                    <label htmlFor="password">パスワード</label>
+                    <input type="text" name="password" id="password" />
+                    <p className='text-red-500' aria-live='polite'>{state?.password}</p>
+                </div>
                 <SubmitButton />
             </form>
         </div>
     )
 }
 
-function SubmitButton () {
+function SubmitButton() {
     const { pending } = useFormStatus();
     return (
         <button type="submit" aria-disabled={pending}>追加</button>
     )
-}
+} 
