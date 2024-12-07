42c37b0b083630327ad94136ddea7e4fc78bca6d
HirayamaSachi <hirayamas0305@gmail.com>
2024-12-07 21:11:47 +0900

    add:該当ユーザがない場合

--- src/app/dashboard/user/[user]/page.tsx
+++ src/app/dashboard/user/[user]/page.tsx
@@ -4,7 +4,8 @@ import { sql } from "../../../../../node_modules/@vercel/postgres/dist/index.cjs
 import { QueryResultRow } from "../../../../../node_modules/@vercel/postgres/dist/index.cjs"
 export default async function Detail({params, }: {params: Promise<{ user : string}>}) {
     const data = await sql`SELECT * FROM users WHERE id = ${(await params).user}`
-    const { rows: users } : { rows : QueryResultRow[] } = data
+    const [UserRow]: QueryResultRow[] = data.rows
+    if(!UserRow) return <main><p>404 NOT FOUND</p></main>
     return (
         <main>
             <p>user詳細</p>
@@ -15,13 +16,15 @@ export default async function Detail({params, }: {params: Promise<{ user : strin
                             <th>id</th>
                             <th>name</th>
                             <th>email</th>
+                            <th>edit user</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td>{users[0].id}</td>
-                            <td>{users[0].name}</td>
-                            <td>{users[0].email}</td>
+                            <td>{UserRow.id}</td>
+                            <td>{UserRow.name}</td>
+                            <td>{UserRow.email}</td>
+                            <td><Link href={`/dashboard/edit/${UserRow.id}`}>編集する</Link></td>
                         </tr>
                     </tbody>
                 </table>
