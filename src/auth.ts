f91856058572aea0fe6b7c813463751335b607fa
HirayamaSachi <hirayamas0305@gmail.com>
2025-01-03 22:34:32 +0900

    add:検証を追加

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import { authConfig } from './auth.config'
 import Credentials from 'next-auth/providers/credentials'
 import { sql } from '@vercel/postgres'
 import bcrypt from "bcrypt";
+import { z }from 'zod'
 
 
 async function getUser(email: string) {
@@ -20,10 +21,14 @@ export const { auth, signIn, signOut } = NextAuth({
     providers: [
         Credentials({
             async authorize(credentials) {
-                const user = await getUser(credentials.email)
-                if (!user) return null
-                const passwordMatch = await bcrypt.compareSync(credentials.password, user.password)
-                if(passwordMatch) return user
+                const parsedCredentials = z.object({ email: z.string(), password: z.string().min(6) }).safeParse(credentials)
+                if (parsedCredentials.success) {
+                    const { email, password } = parsedCredentials.data
+                    const user = await getUser(email)
+                    if (!user) return null
+                    const passwordMatch = await bcrypt.compareSync(password, user.password)
+                    if (passwordMatch) return user
+                }
                 return null
             }
         })]
