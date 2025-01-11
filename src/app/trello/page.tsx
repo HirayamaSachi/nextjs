4d47fa7f2ad14d8c249e64952d8825a7dd012cfc
HirayamaSachi <hirayamas0305@gmail.com>
2025-01-11 16:38:34 +0900

    add:変数をMapで管理

--- src/app/trello/page.tsx
+++ src/app/trello/page.tsx
@@ -21,27 +21,34 @@ import Droppable from './ui/droppable'
 import SortableItemList from './ui/sortableItemList';
 
 export default function Page() {
-    const [draggableItems, setDraggableItems] = useState([
-    ])
 
-    const [tasks, setTasks] = useState([
-        { id: '1', name: "タスク1", parentId: '3' },
-        { id: '2', name: "タスク2", parentId: '1' },
-        { id: '3', name: "タスク3", parentId: '1' },
-    ])
-    const [containers, setContainers] = useState([
-        {id: '1', name: 'todo', color: 'red'},
-        { id: '2', name: 'process', color: 'yellow' },
-        { id: '3', name: 'complete', color: 'blue' }
-    ])
-    const [lists, setLists] = useState(
-        containers.map((container) => {
-            return {
-                ...container,
-                items: tasks.filter(task => task.parentId == container.id)
+    const taskMap = new Map()
+    taskMap.set('1', { name: "タスク1", parentId: '3' })
+    taskMap.set('2', { name: "タスク2", parentId: '1' })
+    taskMap.set('3', { name: "タスク3", parentId: '1' })
+
+    const containerMap = new Map()
+    containerMap.set('1', { name: 'todo', color: 'red' })
+    containerMap.set('2', { name: 'process', color: 'yellow' })
+    containerMap.set('3', { name: 'complete', color: 'blue' })
+
+    const listMap = new Map()
+    containerMap.forEach((container, c_key) => {
+        const tasksByContainer = new Map()
+        taskMap.forEach((task, t_key) => {
+            if (task.parentId == c_key) {
+                tasksByContainer.set(t_key, task)
             }
-        }) 
-    )
+        })
+        listMap.set(c_key, {
+            ...container,
+            items: tasksByContainer
+        })
+    })
+
+    const [tasks, setTasks] = useState(taskMap)
+    const [containers, setContainers] = useState(containerMap)
+    const [lists, setLists] = useState(listMap)
     const sensors = useSensors(
         useSensor(PointerSensor),
         useSensor(KeyboardSensor, {
@@ -49,35 +56,40 @@ export default function Page() {
         })
     )
     const handleDragEnd = (event: DragEndEvent) => {
-        // todo:更新周りの処理
         const { active, over } = event
         if (over) {
-            const activeTask = tasks.find(task => task.id === active.id)
-            if(activeTask == undefined) return
-            if(activeTask.parentId !== over.id) {
-                const newLists =lists.map((list) => {
-                    if(list.id === activeTask.parentId) {
-                        // todo:削除
-                        return {...list, items: []}
-                    }else if( list.id === over.id ) {
-                        // todo:追加
-                        return {...list, items: []}
-                    }else {
-                        return list
-                    }
+            const activeTask = tasks.get(active.id)
+            if (activeTask == undefined) return
+
+            // 別の要素に移動させた場合更新
+            if (activeTask.parentId !== over.id) {
+                const updateTasks = new Map(tasks)
+                const updateLists = new Map()
+                updateTasks.set(active.id, { ...activeTask, parentId: over.id })
+                setTasks(updateTasks)
+                containers.forEach((container, c_key) => {
+                    const tasksByContainer = new Map()
+                    updateTasks.forEach((task, t_key) => {
+                        if (task.parentId == c_key) {
+                            tasksByContainer.set(t_key, task)
+                        }
+                    })
+                    updateLists.set(c_key, {
+                        ...container,
+                        items: tasksByContainer
+                    })
                 })
-                // todo:更新
-                // setLists()
+                setLists(updateLists)
             }
         }
         // todo:arrayMove
     }
     return (
         <DndContext onDragEnd={handleDragEnd} sensors={sensors} collisionDetection={closestCenter}>
-            {lists.map((list) => (
-                <Droppable id={list.id} key={list.id}>
-                    <SortableContext items={draggableItems} strategy={verticalListSortingStrategy}>
-                        <SortableItemList items={list.items} color={list.color} name={list.name}></SortableItemList>
+            {Array.from(lists.entries()).map(([key, list]) => (
+                <Droppable id={key} key={key}>
+                    <SortableContext items={Array.from(list.items.values())} strategy={verticalListSortingStrategy}>
+                        <SortableItemList id={key} items={list.items} color={list.color} name={list.name}></SortableItemList>
                     </SortableContext>
                 </Droppable>
             ))}
--- src/app/trello/ui/sortableItemList.tsx
+++ src/app/trello/ui/sortableItemList.tsx
@@ -1,14 +1,14 @@
 import SortableItem from './sortable';
 import Draggable from './draggable'
-export default function SortableItemList(props: {color: string, name: string, items:{id: string, name: string}[]}) {
+export default function SortableItemList(props: { id: string, color: string, name: string, items: { id: string, name: string }[] }) {
     return (
-        <div className={`bg-${props.color}-50 p-10`}>
+        <div key={props.id} id={props.id} className={`bg-${props.color}-50 p-10`}>
             <h1>{props.name}</h1>
             {
-                props.items?.map((item: { id: string, name: string }) => (
-                    <SortableItem key={item.id} id={item.id}>
+                Array.from(props.items?.entries()).map(([key, item]) => (
+                    <SortableItem key={key} id={key.toString()}>
                         <div className='m-10'>
-                            <Draggable key={item.id} id={item.id}>{item.name}</Draggable>
+                            <Draggable id={key.toString()}>{item.name}</Draggable>
                         </div>
                     </SortableItem>
                 ))
